b31c62a6afa42ef7dc4a7f0d6ef8b928555479a3
Lucius <hellolucius@protonmail.com>
2021-07-31 15:45:13 -0500

    add ability to accept command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // recieve an iteration of the provided command line arguments
+    // and collect them into a vector
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }
