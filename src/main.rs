22f87b30b8e61f084d6bba4097a287522b4608b0
Lucius <hellolucius@protonmail.com>
2021-07-31 17:34:56 -0500

    error handling on file reading

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+// This code is written as a simple project to learn rust
+// most code will be overly commented as I am noting things to myself
+
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -7,6 +11,9 @@ fn main() {
     // and collect them into a vector
     let args: Vec<String> = env::args().collect();
 
+    // unwrap_or_else will, on success, unwrap the Ok() and return the
+    // value inside, or upon fail, it will run the closure
+    // anon function we passed into it.
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
@@ -15,7 +22,14 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    // does not use unwrap_or_else because run() does not
+    // return any value to unwrap on success
+    // so we only check if the result is Err
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -28,6 +42,9 @@ impl Config {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
+
+        // This part could be made more efficent using a different method
+        // I will change later when I learn how
         let query = args[1].clone();
         let filename = args[2].clone();
 
@@ -35,8 +52,10 @@ impl Config {
     }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename).expect("Unable to read file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With Text:\n{}", contents);
+
+    Ok(())
 }
