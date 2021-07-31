975404f247e32a64cb9e33d9cca6a13fcbd553c7
Lucius <hellolucius@protonmail.com>
2021-07-31 15:48:51 -0500

    store arguments in variables

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,10 @@ fn main() {
     // recieve an iteration of the provided command line arguments
     // and collect them into a vector
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
