64b400df6c98ca2476e759282eabc40f4f826e8e
Lucius <hellolucius@protonmail.com>
2021-07-31 16:54:30 -0500

    added error handling for accepts arguments from command line

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // recieve an iteration of the provided command line arguments
     // and collect them into a vector
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -22,10 +26,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
