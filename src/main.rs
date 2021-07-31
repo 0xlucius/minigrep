4fcbecc4ad99e96f706c54444bff600131c902d9
Lucius <hellolucius@protonmail.com>
2021-07-31 16:07:40 -0500

    move argument variable into a stuct to improve program structure

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,24 @@ fn main() {
     // and collect them into a vector
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Unable to read file1");
+    let contents = fs::read_to_string(config.filename).expect("Unable to read file1");
 
     println!("With Text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
